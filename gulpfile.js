b8ece5752e62f2b836aa2d3aa2f7118347ebea29
abr-ya <gamer05@list.ru>
2021-03-30 22:48:50 +0300

    feat: Add PUG, Panda's and Gorilla's pages (empty)

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,19 @@ const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 
-// pug отключаю пока
-// gulp.task('pug-compile', () => {
+// pug
+gulp.task('pug-panda', () => {
+  return gulp.src('src/pug/panda/index.pug').pipe(pug({pretty:true}))
+    .pipe(gulp.dest('pages/zoos/panda/')).pipe(browserSync.stream())
+});
+
+gulp.task('pug-gorilla', () => {
+  return gulp.src('src/pug/gorilla/index.pug').pipe(pug({pretty:true}))
+    .pipe(gulp.dest('pages/zoos/gorilla/')).pipe(browserSync.stream())
+});
+
+// pug BU
+// gulp.task('pug-panda', () => {
 //   return gulp.src([
 //     'src/**/*.pug',
 //     '!src/components/**/*.pug',
@@ -29,12 +40,28 @@ gulp.task('sass-map', () => {
     .pipe(browserSync.stream())
 });
 
+gulp.task('sass-zoo', () => {
+  return gulp.src('src/pug/**/*.scss')
+    .pipe(sass())
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('pages/zoos/'))
+    .pipe(browserSync.stream())
+});
+
+// при изменении компонента, шаблона или scss в папке
+const pugWatch = [
+  'src/pug/components/**/*.*',
+  'src/pug/templates/zoo.pug',
+  'src/**/*.scss',
+]
+
 gulp.task('default', () => {
   browserSync.init({
     server: './',
     port: 3010
   });
-  // gulp.watch('src/**/*.pug', gulp.series('pug-compile'));
+  gulp.watch([...pugWatch, 'src/pug/panda/index.pug'], gulp.series('pug-panda'));
+  gulp.watch([...pugWatch, 'src/pug/gorilla/index.pug'], gulp.series(['pug-gorilla', 'sass-zoo']));
   gulp.watch('src/scss/landing.scss', gulp.series('sass-landing'));
   gulp.watch('src/scss/map.scss', gulp.series('sass-map'));
   gulp.watch('**/*.html').on('change', browserSync.reload);
